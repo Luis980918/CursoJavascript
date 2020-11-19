77becbae336eb580228e95af00ee35bdf09b28ad
Luis Carlos <lcarlos.rendon@udea.edu.co>
2020-11-19 16:11:44 -0500

    Creando archivos js y html

---
+++ app.js
@@ -0,0 +1,47 @@
+/*
+Tipos de datos
+Todos estos tipos de datos se pueden generar o ver en
+consola utilizando el codigo console.log(tipodato);
+
+Ejemplo con un objeto:
+console.log({
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true,
+    "friends": ['joe','ryan', 'martha']
+});
+
+"Helo World" => string
+'Helo World' => string
+
+1000 =>number
+
+true-false =>boolean
+
+['joe','ryan', 'martha'] =>array
+
+Object
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true,
+    "friends": ['joe','ryan', 'martha']
+}
+
+Variables
+Antes de declararse es necesario poner la palabra 'var'
+o la palabra 'let', de la siguiente manera:
+
+var nombre="John";
+let lastname="Carter"
+console.log(nombre+" "+lastname);
+
+
+*/
+
+var nombre="John";
+let lastname="Carter"
+console.log(nombre+" "+lastname);
+
